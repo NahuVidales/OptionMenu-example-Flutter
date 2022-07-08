7a039a9e16babe5f106818627f82f2b484188b78
elNahu-rgb <83135275+elNahu-rgb@users.noreply.github.com>
2022-07-07 21:31:16 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter_application_1/screens/screens.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(
+      const MyApp(),
+    );
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
